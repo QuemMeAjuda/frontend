368c285ae1dbda5d2586646271085b6f2d5260c4
Yovany Cunha <yovany33@gmail.com>
2018-05-16 17:53:57 -0300

    tela de cadastro de tutor, ainda falta o css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,12 +20,14 @@ import { AuthComponent } from './auth/auth.component';
 import { AuthGuard} from './auth/auth.guard';
 import { AuthService } from './auth/auth.service';
 import { HomeComponent } from './home/home.component';
+import { TutorRegisterComponent } from './tutor-register/tutor-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    HomeComponent
+    HomeComponent,
+    TutorRegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tutor-register/tutor-register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutor-register',
+  templateUrl: './tutor-register.component.html',
+  styleUrls: ['./tutor-register.component.css']
+})
+export class TutorRegisterComponent implements OnInit {
+
+  protected skills: any;
+  private out: Array<string> = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public becameTutor() {
+    this.skills = this.skills.split(" ");
+    this.skills = this.removeSpacesFromSkillsArray(this.skills);
+  }
+
+  private removeSpacesFromSkillsArray(skillsArray) {
+    this.out = [];
+    skillsArray.forEach((element) => {
+      if (element !== "") {
+        this.out.push(element);
+      }
+    });
+    return this.out;
+  }
+
+
+
+}
