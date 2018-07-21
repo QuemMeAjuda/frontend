38e78205689928fb4d6b719f6d6ab7f3fcccf09b
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2018-07-21 11:53:32 -0300

    Finishing

--- src/app/rank-users/rank-users.component.ts
+++ src/app/rank-users/rank-users.component.ts
@@ -14,6 +14,25 @@ export class RankUsersComponent implements OnInit {
   constructor(private userService: UserService,
     private router: Router) { }
 
+  getPhoto(obj) {
+    const url = obj && obj.photoURL || 'https://img.meutimao.com.br/_upload/forumtopico/2017/03/13/qeqv9vj.png';
+    return `url(${url})`;
+  }
+
+  getRate(currentUser): any{
+    let total = currentUser && currentUser.evaluation.reduce((a,b) => {
+      a+=b.rating;
+      return a;
+    },0);
+    let result = (total / (currentUser && currentUser.evaluation.length))
+    return result && result.toFixed(2) || "Não avaliado ainda";
+  }
+
+  goToUserPage(user) {
+    let id = user && user._id;
+    this.router.navigate(['/user_details', id]);
+  }
+
   ngOnInit() {
     this.userService.getTopUsers().subscribe(res=> {
       this.users = res['data'];
